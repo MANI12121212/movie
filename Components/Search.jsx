2409a6a89d0e423f12d69aa97ac351c3ba9f8487
MANI12121212 <101326938+MANI12121212@users.noreply.github.com>
2023-03-09 23:32:48 +0530

    movie

---
+++ Components/Search.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "./search.css"
+
+const Search = (props) => {
+  const [searchValue, setSearchValue] = useState("");
+  
+  const handleSearchInputChanges = (e) => {
+    setSearchValue(e.target.value);
+  }
+
+  const resetInputField = () => {
+    setSearchValue("")
+  }
+
+  const callSearchFunction = (e) => {
+    e.preventDefault();
+    props.search(searchValue);
+    resetInputField();
+  }
+
+  return (
+      <form className="search">
+        <input className="input"
+          value={searchValue}
+          onChange={handleSearchInputChanges}
+          type="text"
+        />
+        <input onClick={callSearchFunction} type="submit" value="SEARCH" />
+      </form>
+    );
+}
+
+export default Search;
